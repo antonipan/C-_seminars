39a636b7debf55f98f30530e4016db81a2bd4b98
Anton P <a.s.panferov@mail.ru>
2022-10-13 20:34:20 +0300

    seminar 8

--- C#8/Program.cs
+++ C#8/Program.cs
@@ -1,2 +1,132 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+// Задача 53: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+
+int [,] FillArray ()
+{
+    Console.Write("Введите число строк: ");
+    int row = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите число стоблцов: ");
+    int column = Convert.ToInt32(Console.ReadLine());
+    int [,] array = new int [row, column]; 
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array [i,j] = new Random().Next(0,10);
+        }
+    }
+    return array;
+}
+
+void PrintArray (int [,] array)
+{
+    
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array [i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int [,] mass1 = FillArray();
+PrintArray(mass1);
+int [,] ReplaceLines(int [,] array)
+{
+    int replace;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == 0) 
+            {
+                replace = array [i,j];
+                array [i,j] = array [array.GetLength(0)-1, j];
+                array [array.GetLength(0)-1, j] = replace;
+            }
+        }
+    }
+    return array;
+}
+Console.WriteLine();
+ReplaceLines(mass1);
+PrintArray(mass1);
+
+// Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+Console.WriteLine("Задача 55");
+
+int [,] ReplaceRowColumn (int [,] array)
+{
+    int row = array.GetLength(1);
+    int column = array.GetLength(0);
+    int [,] array2 = new int [row, column];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+          array2 [j,i] = array[i,j];
+        }
+    }
+    return array2;
+}
+
+int [,] mass2 = ReplaceRowColumn(mass1);
+PrintArray(mass2);
+// Задача 57: Составить частотный словарь элементов двумерного массива. 
+// Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+// Набор данных
+// Частотный массив
+// { 1, 9, 9, 0, 2, 8, 0, 9 }
+// 0 встречается 2 раза 
+// 1 встречается 1 раз 
+// 2 встречается 1 раз 
+// 8 встречается 1 раз 
+// 9 встречается 3 раза
+// 1, 2, 3 
+// 4, 6, 1 
+// 2, 1, 6
+// 1 встречается 3 раза 
+// 2 встречается 2 раз 
+// 3 встречается 1 раз 
+// 4 встречается 1 раз 
+// 6 встречается 2 раза
+Console.WriteLine("Задача 57");
+
+void Frequency (int [,] array)
+{
+    for (int m = 0; m < 10; m++)
+    {
+        int count = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {                    
+                if (array[i,j] == m) count++;
+                
+                
+            }
+        }
+        Console.WriteLine ($"число {m} = {count} раз");
+        
+    }    
+
+}
+Frequency(mass2);
+// Задача 59: Задайте двумерный массив из целых чисел. Напишите программу, которая удалит строку и столбец, на пересечении которых расположен наименьший элемент массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Наименьший элемент - 1, на выходе получим 
+// следующий массив:
+// 9 4 2
+// 2 2 6
+// 3 4 7
+
+
+
+// Задача 61: Вывести первые N строк треугольника Паскаля. Сделать вывод в виде равнобедренного треугольника
---
+++ C#8_homework/Program.cs
@@ -0,0 +1,226 @@
+﻿Console.Clear();
+
+Console.WriteLine("Задача 54");
+// Задайте двумерный массив. Напишите программу, 
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+
+int [,] FillArray()
+{
+    Console.WriteLine("Введите размер массива");
+    Console.Write("Строки: ");
+    int row = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Столбцы: ");
+    int column = Convert.ToInt32(Console.ReadLine());
+    int [,] array = new int [row, column];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            array[i,j] = new Random().Next(0,5);
+        }
+    }
+    return array;
+}
+
+void Printarray(int [,]array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int [,] mass1 = FillArray();
+Printarray(mass1);
+
+int [,] Minrowarray (int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int index = 0;
+        while(index < array.GetLength(1))
+        {    
+            int max = array [i, index];
+            for (int j = index+1; j < array.GetLength(1); j++)
+            {
+                if (array [i,j] > max)
+                {
+                    max = array[i,j];
+                    array[i,j] = array[i, index];
+                    array[i, index] = max;
+                }
+            }
+            index++;
+        }
+    }
+    return array;
+}
+
+int [,] mass2 = Minrowarray(mass1);
+Console.WriteLine();
+Printarray(mass2);
+// int [] array = {4, 5, 3, 8, 6};
+// int index = 0;
+// while(index < array.Length)
+// {    
+//     int max = array[index];
+//     for (int i = index+1; i < array.Length; i++)
+//     {
+//         if (array[i] > max) 
+//         {
+//             max = array[i];
+//             array[i] = array[index];
+//             array[index] = max;
+//         }
+//     }
+//     index++;
+// }
+// for (int i = 0; i < array.Length; i++)
+// {
+//     Console.Write($"{array[i]} ");
+    
+Console.WriteLine("Задача 56");
+// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
+// которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке 
+// и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+int Rowminsumm (int [,] array)
+{
+    int [] array2 = new int [array.GetLength(0)];
+    
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int summ = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            summ = summ + array [i,j];
+        }
+        array2[i] = summ;
+        Console.Write($"{array2[i]} ");
+    }
+    int min = array2[0];
+    int index = 1;
+    for (int m = 0; m < array2.Length; m++ )
+    {
+        if (array2[m] < min) 
+        {
+            min = array2[m];
+            index = m+1;
+        }
+    }
+    return index;
+}
+
+Console.WriteLine();
+int index = Rowminsumm(mass1);
+Console.WriteLine();
+if (index == 3)
+{
+    Console.WriteLine($"В {index}-ей строке минимальная сумма");
+} else
+{
+    Console.WriteLine($"В {index}-ой строке минимальная сумма");
+
+}
+
+Console.WriteLine("Задача 58");
+Console.WriteLine("Матрица А");
+int [,] mass3 = FillArray();
+Printarray(mass3);
+Console.WriteLine();
+Console.WriteLine("Матрица B");
+int [,] mass4 = FillArray();
+Printarray(mass4);
+Console.WriteLine();
+
+int [,] Umnogeniematrix (int [,] array1, int [,] array2)
+{
+  
+    int row1 = array1.GetLength(0);
+    int column1 = array1.GetLength(1);
+    int row2 = array2.GetLength(0);
+    int column2 = array2.GetLength(1);
+    int [,] array3 = new int [row1, column2];
+    if (row1 != column2) 
+        {
+            Console.WriteLine ("Матрицы нельзя перемножить");
+        }  
+    else {
+            for (int i = 0; i < row1; i++)
+            {
+                for (int j = 0; j < column2; j++)
+                {
+                    for (int m = 0; m < row2; m++)
+                    {
+                        {
+                        array3 [i,j] += array1 [i, m] * array2 [m,j];
+                        }              
+                    }
+                }
+            }
+        }
+    return array3;
+        
+}
+int [,] mass5 = Umnogeniematrix(mass3, mass4);
+Console.WriteLine ("Итоговая матрица C = A*B");
+Printarray(mass5);
+
+
+// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+Console.WriteLine("Задача 58");
+
+int [,,] FillthreeArray()
+{
+    int [,,] array = new int [2,2,2];
+    for (int x = 0; x < 2; x++)
+    {
+        for (int y = 0; y < 2; y++)
+        {
+            for (int z = 0; z < 2; z++)
+            {
+                array[x,y,z] = new Random().Next(10, 100);
+            }
+        }
+    }
+    return array;
+}
+
+int [,,] Norepeatarray (int [,,] array)
+{
+    for (int x = 0; x < array.GetLength(0); x++)
+    {
+        for (int y = 0; y < array.GetLength(1); y++)
+        {
+            for (int z = 0; z < array.GetLength(2); z++)
+            {
+                array[x,y,z] = new Random().Next(10, 100);
+            }
+        }
+    }
+    return array;
+} 
+}
+
+// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
